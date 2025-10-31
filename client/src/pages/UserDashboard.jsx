8d20a7aa2bfd180f5805300c0d19855b7504fa4d
martins0909 <158082763+martins0909@users.noreply.github.com>
2025-10-31 07:02:34 +0100

    Fix API endpoint variable

--- client/src/pages/UserDashboard.jsx
+++ client/src/pages/UserDashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import BblogComments from "../components/BblogComments";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
 
 
 const UserDashboard = () => {
